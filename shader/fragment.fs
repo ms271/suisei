2b93d8054ad08890d1f90aff56137e5a56378a92
Manu <manurajsingh271@gmail.com>
2026-02-01 04:45:34 +0530

    removed dual texture support from fragment shader

--- shader/fragment.fs
+++ shader/fragment.fs
@@ -8,5 +8,5 @@ uniform sampler2D ourTexture2;
 
 void main()
 {
-    FragColor = mix(texture(ourTexture1, TexCoord), texture(ourTexture2, TexCoord), 0.5);
+    FragColor = texture(ourTexture1, TexCoord);
 };
