454cb2bcbdefc649163b76e31bc6632ee766d6bb
Manu <manurajsingh271@gmail.com>
2026-03-12 01:53:29 +0530

    fixed material.diffuse

--- shader/fragment.fs
+++ shader/fragment.fs
@@ -64,7 +64,7 @@ void main()
         vec3 norm = normalize(Normal);
         vec3 lightDir = normalize(light.position - FragPos);
         float diff = max(dot(norm, lightDir), 0.0);
-        vec3 diffuse = (diff * material.diffuse) * light.ambient;
+        vec3 diffuse = (diff * material.diffuse) * light.diffuse;
     // specular in camPos, FragPos, material, light
         vec3 viewDir = normalize(camPos - FragPos);
         vec3 reflectDir = reflect(-lightDir, norm);
