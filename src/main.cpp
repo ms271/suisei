bf6ee56463f6566f725a0a154b979bdf32affb02
Manu <manurajsingh271@gmail.com>
2026-03-31 08:37:41 +0530

    Stripped unneeded stuff, from the scene

--- src/main.cpp
+++ src/main.cpp
@@ -35,108 +35,20 @@ int main()
     mesh cubeMesh;
     cubeMesh.buffer();
 
-    texture texture1(1, "textures/container2.png");
-    texture texture2(2, "textures/container2_specular.png");
-    texture texture3(3, "textures/wall.jpg");
-
     object cube1;
-    //set coords
-    cube1.p.clear();
-    for (int i = -5; i <= 5; i++)
-    {
-        for(int j = -5; j <= 5; j++)
-        {
-            cube1.p.push_back(glm::vec3(i, -2, j));
-            cube1.p.push_back(glm::vec3(i, +2, j));
-        }
-    }
-    //set bools
-    cube1.useDiffTex = 1;
-    cube1.useSpecTex = 1;
-    //set tex
-    cube1.material.diffTex = &texture1;
-    cube1.material.specTex = &texture2;
-    //set mesh
+    
     cube1.objMesh = &cubeMesh;
-    //set draw type
     cube1.drawWorld = &simpleWorldDraw;
 
-    object cube2;
-    cube2.p[0] = glm::vec3(10.0f, 0.0f, 0.0f);
-    cube2.objMesh = &cubeMesh;
-    cube2.material.mainVec = glm::vec3(1);
-    cube2.material.ambVec = glm::vec3(1);
-    cube2.material.diffVec = glm::vec3(1);
-    cube2.material.specVec = glm::vec3(1);
-
-    cube2.flatShade = 1;
-    cube2.drawWorld = &simpleWorldDraw;
-
-    object cube4;
-    cube4.p[0] = glm::vec3(0.0f, 10.0f, 0.0f);
-    cube4.objMesh = &cubeMesh;
-    cube4.material.mainVec = glm::vec3(1);
-    
-    cube4.flatShade = 1;
-    cube4.drawWorld = &simpleWorldDraw;
-
-    object cube3;
-    cube3.p.clear();
-    for (int i = -5; i <= +5; i++)
-    {
-        for(int j = -1; j <= +1; j++)
-        {
-            cube3.p.push_back(glm::vec3(i, j, 5));
-            cube3.p.push_back(glm::vec3(i, j, -5));
-            cube3.p.push_back(glm::vec3(5, j, i));
-            cube3.p.push_back(glm::vec3(-5, j, i));
-        }
-    }
-    cube3.useDiffTex = 1;
-    //set tex
-    cube3.material.diffTex = &texture3;
-    //set mesh
-    cube3.objMesh = &cubeMesh;
-    //set draw type
-    cube3.drawWorld = &simpleWorldDraw;
-    cube3.material.specVec = glm::vec3(0.2, 0.2, 0.2);
-
-    //object cube4;
-    //cube4.p.clear();
-    //for (int i = 0; i < cubePositions.size(); i++)
-    //{
-    //    cube4.p.push_back(cubePositions[i] + glm::vec3(0, 0, 2));
-    //    cube4.p.push_back(-(cubePositions[i] + glm::vec3(0, 0, 2)));
-    //}
-    //cube4.useDiffTex = 1;
-    //cube4.useSpecTex = 1;
-    ////set tex
-    //cube4.material.diffTex = &texture1;
-    //cube4.material.specTex = &texture2;
-    ////set mesh
-    //cube4.objMesh = &cubeMesh;
-    ////set draw type
-    //cube4.drawWorld = &simpleWorldDraw;
-
-    flashLgt flashLight;
-    posLgt posLight1;
-    posLgt posLight2;
+    dirLgt ambLight;
+    ambLight.ambient = glm::vec3(0.3);
+    ambLight.diffuse = glm::vec3(0);
+    ambLight.specular = glm::vec3(0);
 
     light light1;
-    light1.flashLight = &flashLight;
-    light1.lightType = 2;
-    cam1.camLight = light1.flashLight;
+    light1.dirLight = &ambLight;
+    light1.lightType = 1;
     
-    light light2;
-    light2.posLight = &posLight1;
-    light2.index = 0;
-    light2.posLight->position = cube2.p[0];
-
-    light light3;
-    light3.posLight = &posLight2;
-    light3.index = 1;
-    light3.posLight->position = cube4.p[0];
-
     stbi_set_flip_vertically_on_load(true);
 
     ourShader.use(); 
@@ -163,12 +75,7 @@ int main()
         ourShader.setMat4("projection", cam1.PROJ);
 
         light1.run(ourShader);
-        light2.run(ourShader);
-        light3.run(ourShader);
-
-        cube2.draw(model, ourShader, cam1);
-        cube4.draw(model, ourShader, cam1);
-        cube3.draw(model, ourShader, cam1);
+        
         cube1.draw(model, ourShader, cam1);
 
         glfwSwapBuffers(ourWindow.window);
